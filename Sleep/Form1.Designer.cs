f2a05f97ea143eb5d6476fadb8437c760d9f69c1
Владислав <vladislav.zhilin@mail.ru>
2020-09-11 11:50:41 +0300

    Добавьте файлы проекта.

---
+++ Sleep/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Sleep
+{
+    partial class SleepSetForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SleepSetForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите, через сколько времени отправить компьютер в сон";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(15, 30);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox1.TextChanged += new System.EventHandler(this.maskedTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(122, 30);
+            this.label2.MaximumSize = new System.Drawing.Size(200, 50);
+            this.label2.MinimumSize = new System.Drawing.Size(200, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 50);
+            this.label2.TabIndex = 4;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Hibernate timer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // SleepSetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 97);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SleepSetForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SleepSetForm_FormClosing);
+            this.Load += new System.EventHandler(this.SleepSetForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Sleep/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Sleep
+{
+    public partial class SleepSetForm : Form
+    {
+        private System.Threading.Timer hibernateTimer;
+        private System.Threading.Timer prepareTimer;
+
+        private TimeSpan timerSpan;
+
+        public SleepSetForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SleepSetForm_Load(object state, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string timeText = this.maskedTextBox1.Text;
+
+            if (TimeSpan.TryParseExact(timeText, "g", System.Globalization.CultureInfo.InvariantCulture, out TimeSpan timeSpan))
+            {
+                timerSpan = timeSpan;
+                label2.Text = $"Компьютер будет переведен в спящий режим {DateTime.Now + timerSpan:G}";
+            }
+            else
+            {
+                timerSpan = TimeSpan.MaxValue;
+                label2.Text = "Неверный формат времени";
+            }
+        }
+
+        private void OnHibernate(object sender)
+        {
+            bool can = Application.SetSuspendState(PowerState.Hibernate, false, false);
+            if(!can)
+                can = Application.SetSuspendState(PowerState.Suspend, false, false);
+
+            if(!can)
+            {
+                this.notifyIcon1.BalloonTipText = "Ошибка";
+                this.notifyIcon1.BalloonTipTitle = $"Не удалось перевести компьютер в спящий режим";
+                this.notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+
+                this.notifyIcon1.ShowBalloonTip(3000);
+            }
+
+            hibernateTimer?.Dispose();
+        }
+
+        private void MessageOnHibernate(object sender)
+        {
+            this.notifyIcon1.BalloonTipText = "Уведомление";
+            this.notifyIcon1.BalloonTipTitle = $"Ваш компьютер будет переведен в спящий режим через 5 минут";
+            this.notifyIcon1.BalloonTipIcon = ToolTipIcon.Warning;
+
+            this.notifyIcon1.ShowBalloonTip(3000);
+        }
+
+        private void SleepSetForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.notifyIcon1.BalloonTipText = "Уведомление";
+            this.notifyIcon1.BalloonTipTitle = $"Таймер выключения остановлен";
+            this.notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+
+            this.notifyIcon1.ShowBalloonTip(3000);
+
+            prepareTimer?.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timerSpan != null)
+            {
+                SetupTimer();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void SetupTimer()
+        {
+            this.Hide();
+
+            this.notifyIcon1.BalloonTipText = "Уведомление";
+            this.notifyIcon1.BalloonTipTitle = $"Компьютер будет переведен в спящий режим {DateTime.Now + timerSpan:G}";
+            this.notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+
+            this.notifyIcon1.ShowBalloonTip(3000);
+
+            //hibernateTimer = new System.Threading.Timer(OnHibernate, null, timerSpan, TimeSpan.Zero);
+            if(timerSpan != TimeSpan.MaxValue)
+            {
+                if(timerSpan.TotalMinutes > 5d)
+                {
+                    prepareTimer = new System.Threading.Timer(MessageOnHibernate, null, timerSpan - TimeSpan.FromMinutes(5), TimeSpan.Zero);
+                }
+            }
+        }
+    }
+}
